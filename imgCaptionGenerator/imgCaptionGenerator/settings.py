a34e99d7c7f724fe03d9cbc756159743520c2096
ANUSHA P V <121385976+Anusha629@users.noreply.github.com>
2023-09-30 17:25:46 +0530

    Update settings.py

--- imgCaptionGenerator/imgCaptionGenerator/settings.py
+++ imgCaptionGenerator/imgCaptionGenerator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1i(1@b*7&2z8fd51ip2(rg$f3xnayuk69vnp=-%)20##v56u+t'
+SECRET_KEY = 'Your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
